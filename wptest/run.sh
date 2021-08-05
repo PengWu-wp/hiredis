9428abaf6a2f1652346a6e8c51c455c3d86b8509
PengWu-wp <523972511@qq.com>
2021-08-05 00:19:00 -0700

    add script

--- wptest/run.sh
+++ wptest/run.sh
@@ -1,151 +1,37 @@
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test &
-./test & 
-./test &
-./test &  
-./test & 
-./test &
-./test &  
-./test & 
-./test &  
-./test & 
 
+
+
+while getopts "n:h" optname
+do
+    case "$optname" in
+      "n")
+        CLIENTNUM=$OPTARG
+        ;;
+       "h")
+        echo "eg:./run.sh -n <client_num>"
+	exit 0
+        ;;
+      ":")
+        echo "No argument value for option $OPTARG"
+        ;;
+      "?")
+        echo "Unknown option $OPTARG"
+        ;;
+      *)
+        echo "Unknown error while processing options"
+        ;;
+    esac
+done
+
+if [ $# -eq 0 ]
+then
+	echo "client_num not specified! eg:./run.sh -n <client_num>"
+	exit -1
+fi
+
+N=0
+while [ $N -lt $CLIENTNUM ]
+do
+	./test &
+	let N++
+done
